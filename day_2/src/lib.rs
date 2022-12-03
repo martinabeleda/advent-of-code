8d376ec45009dc6b72dfa8767fb963cafe643a1d
martinabeleda <martin.abeleda@gmail.com>
2022-12-02 19:12:24 -0800

    Minor refactoring

--- day_2/src/lib.rs
+++ day_2/src/lib.rs
@@ -2,13 +2,13 @@ use std::str::FromStr;
 use std::string::ParseError;
 
 #[derive(Debug)]
-enum HandShape {
+enum Shape {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
 }
 
-impl FromStr for HandShape {
+impl FromStr for Shape {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -25,13 +25,13 @@ impl FromStr for HandShape {
 }
 
 #[derive(Debug)]
-enum Strategy {
+enum Outcome {
     Lose = 0,
     Draw = 3,
     Win = 6,
 }
 
-impl FromStr for Strategy {
+impl FromStr for Outcome {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -39,7 +39,7 @@ impl FromStr for Strategy {
             "X" => Ok(Self::Lose),
             "Y" => Ok(Self::Draw),
             "Z" => Ok(Self::Win),
-            _ => panic!("Problem parsing strategy: {:?}", s),
+            _ => panic!("Problem parsing outcome: {:?}", s),
         }
     }
 }
@@ -48,29 +48,27 @@ pub fn part_a(input: &str) -> i64 {
     let mut score: i64 = 0;
     for line in input.trim().split("\n") {
         let mut split = line.split(" ");
-        let opp_shape = split.next().unwrap().parse::<HandShape>().unwrap();
-        let my_shape = split.next().unwrap().parse::<HandShape>().unwrap();
+        let theirs = split.next().unwrap().parse::<Shape>().unwrap();
+        let ours = split.next().unwrap().parse::<Shape>().unwrap();
 
-        let strategy = match opp_shape {
-            HandShape::Rock => match my_shape {
-                HandShape::Rock => Strategy::Draw,
-                HandShape::Paper => Strategy::Win,
-                HandShape::Scissors => Strategy::Lose,
+        let outcome = match theirs {
+            Shape::Rock => match ours {
+                Shape::Rock => Outcome::Draw,
+                Shape::Paper => Outcome::Win,
+                Shape::Scissors => Outcome::Lose,
             },
-            HandShape::Paper => match my_shape {
-                HandShape::Rock => Strategy::Lose,
-                HandShape::Paper => Strategy::Draw,
-                HandShape::Scissors => Strategy::Win,
+            Shape::Paper => match ours {
+                Shape::Rock => Outcome::Lose,
+                Shape::Paper => Outcome::Draw,
+                Shape::Scissors => Outcome::Win,
             },
-            HandShape::Scissors => match my_shape {
-                HandShape::Rock => Strategy::Win,
-                HandShape::Paper => Strategy::Lose,
-                HandShape::Scissors => Strategy::Draw,
+            Shape::Scissors => match ours {
+                Shape::Rock => Outcome::Win,
+                Shape::Paper => Outcome::Lose,
+                Shape::Scissors => Outcome::Draw,
             },
         };
-        let shape_score = my_shape as i64;
-        let result = strategy as i64;
-        score = score + result + shape_score;
+        score = score + outcome as i64 + ours as i64;
     }
     score
 }
@@ -79,30 +77,27 @@ pub fn part_b(input: &str) -> i64 {
     let mut score: i64 = 0;
     for line in input.trim().split("\n") {
         let mut split = line.split(" ");
-        let opp_shape = split.next().unwrap().parse::<HandShape>().unwrap();
-        let my_strat = split.next().unwrap().parse::<Strategy>().unwrap();
+        let theirs = split.next().unwrap().parse::<Shape>().unwrap();
+        let outcome = split.next().unwrap().parse::<Outcome>().unwrap();
 
-        let my_shape = match opp_shape {
-            HandShape::Rock => match my_strat {
-                Strategy::Win => HandShape::Paper,
-                Strategy::Draw => HandShape::Rock,
-                Strategy::Lose => HandShape::Scissors,
+        let ours = match theirs {
+            Shape::Rock => match outcome {
+                Outcome::Win => Shape::Paper,
+                Outcome::Draw => Shape::Rock,
+                Outcome::Lose => Shape::Scissors,
             },
-            HandShape::Paper => match my_strat {
-                Strategy::Win => HandShape::Scissors,
-                Strategy::Draw => HandShape::Paper,
-                Strategy::Lose => HandShape::Rock,
+            Shape::Paper => match outcome {
+                Outcome::Win => Shape::Scissors,
+                Outcome::Draw => Shape::Paper,
+                Outcome::Lose => Shape::Rock,
             },
-            HandShape::Scissors => match my_strat {
-                Strategy::Win => HandShape::Rock,
-                Strategy::Draw => HandShape::Scissors,
-                Strategy::Lose => HandShape::Paper,
+            Shape::Scissors => match outcome {
+                Outcome::Win => Shape::Rock,
+                Outcome::Draw => Shape::Scissors,
+                Outcome::Lose => Shape::Paper,
             },
         };
-
-        let shape_score = my_shape as i64;
-        let result = my_strat as i64;
-        score = score + result + shape_score;
+        score = score + outcome as i64 + ours as i64;
     }
     score
 }
