74512d86a49549d961f32f224f89242db6834df4
martinabeleda <martin.abeleda@gmail.com>
2022-12-03 09:59:30 -0800

    Refactor day one to use iterators

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,41 +1,40 @@
-use std::collections::BinaryHeap;
-
-pub fn part_a(content: &str) -> i64 {
-    let mut max: i64 = 0;
-    for elf in content.trim().split("\n\n") {
-        let calories: i64 = elf
-            .trim()
-            .split("\n")
-            .map(|l| l.parse::<i64>().unwrap())
-            .sum();
-        if calories > max {
-            max = calories;
-        }
-    }
-    max
+pub fn part_a(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|x| {
+            return x.lines().flat_map(str::parse::<usize>).sum::<usize>();
+        })
+        .max()
+        .unwrap()
 }
 
-pub fn part_b(content: &str) -> i64 {
-    let mut heap = BinaryHeap::with_capacity(3);
-    for elf in content.trim().split("\n\n") {
-        let calories: i64 = elf
-            .trim()
-            .split("\n")
-            .map(|l| l.parse::<i64>().unwrap())
-            .sum();
-        heap.push(calories);
-    }
-    let mut vec = heap.into_sorted_vec();
-    vec.reverse();
-    vec.truncate(3);
-    vec.into_iter().sum()
+pub fn part_b(input: &str) -> usize {
+    let mut vec = input
+        .split("\n\n")
+        .map(|x| {
+            return x.lines().flat_map(str::parse::<usize>).sum::<usize>();
+        })
+        .collect::<Vec<usize>>();
+
+    vec.sort_by(|a, b| b.cmp(a));
+    vec.into_iter().take(3).sum()
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
+    fn part_a_sample() {
+        assert_eq!(super::part_a(include_str!("sample.txt")), 24000);
+    }
+
+    #[test]
     fn part_a() {
         assert_eq!(super::part_a(include_str!("input.txt")), 66616);
+    }
+
+    #[test]
+    fn part_b_sample() {
+        assert_eq!(super::part_b(include_str!("sample.txt")), 45000);
     }
 
     #[test]
