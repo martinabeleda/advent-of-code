4ff14acb38946e62de24686df830abe04c8dc816
martinabeleda <martin.abeleda@gmail.com>
2022-12-02 20:14:16 -0800

    Refactor day 2 to use PartialOrd

--- day_2/src/lib.rs
+++ day_2/src/lib.rs
@@ -1,11 +1,21 @@
 use std::str::FromStr;
-use std::string::ParseError;
+use std::{cmp::Ordering, cmp::PartialOrd, string::ParseError};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Shape {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Shape {
+    fn score(&self) -> usize {
+        match self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
 }
 
 impl FromStr for Shape {
@@ -21,6 +31,23 @@ impl FromStr for Shape {
             "Z" => Ok(Self::Scissors),
             _ => panic!("Problem parsing hand shape: {:?}", s),
         }
+    }
+}
+
+impl PartialOrd for Shape {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let result = match (self, other) {
+            (Shape::Rock, Shape::Rock) => Ordering::Equal,
+            (Shape::Rock, Shape::Paper) => Ordering::Less,
+            (Shape::Rock, Shape::Scissors) => Ordering::Greater,
+            (Shape::Scissors, Shape::Scissors) => Ordering::Equal,
+            (Shape::Scissors, Shape::Paper) => Ordering::Greater,
+            (Shape::Scissors, Shape::Rock) => Ordering::Less,
+            (Shape::Paper, Shape::Paper) => Ordering::Equal,
+            (Shape::Paper, Shape::Rock) => Ordering::Greater,
+            (Shape::Paper, Shape::Scissors) => Ordering::Less,
+        };
+        Some(result)
     }
 }
 
@@ -44,60 +71,49 @@ impl FromStr for Outcome {
     }
 }
 
-pub fn part_a(input: &str) -> i64 {
-    let mut score: i64 = 0;
+fn outcome_from_game(our: &Shape, their: &Shape) -> Outcome {
+    let outcome = if our == their {
+        Outcome::Draw
+    } else if our < their {
+        Outcome::Lose
+    } else {
+        Outcome::Win
+    };
+    outcome
+}
+
+pub fn part_a(input: &str) -> usize {
+    let mut score: usize = 0;
     for line in input.trim().split("\n") {
         let mut split = line.split(" ");
         let theirs = split.next().unwrap().parse::<Shape>().unwrap();
         let ours = split.next().unwrap().parse::<Shape>().unwrap();
 
-        let outcome = match theirs {
-            Shape::Rock => match ours {
-                Shape::Rock => Outcome::Draw,
-                Shape::Paper => Outcome::Win,
-                Shape::Scissors => Outcome::Lose,
-            },
-            Shape::Paper => match ours {
-                Shape::Rock => Outcome::Lose,
-                Shape::Paper => Outcome::Draw,
-                Shape::Scissors => Outcome::Win,
-            },
-            Shape::Scissors => match ours {
-                Shape::Rock => Outcome::Win,
-                Shape::Paper => Outcome::Lose,
-                Shape::Scissors => Outcome::Draw,
-            },
-        };
-        score = score + outcome as i64 + ours as i64;
+        let outcome = outcome_from_game(&ours, &theirs);
+        score = score + outcome as usize + ours.score();
     }
     score
 }
 
-pub fn part_b(input: &str) -> i64 {
-    let mut score: i64 = 0;
+pub fn part_b(input: &str) -> usize {
+    let mut score: usize = 0;
     for line in input.trim().split("\n") {
         let mut split = line.split(" ");
         let theirs = split.next().unwrap().parse::<Shape>().unwrap();
         let outcome = split.next().unwrap().parse::<Outcome>().unwrap();
 
-        let ours = match theirs {
-            Shape::Rock => match outcome {
-                Outcome::Win => Shape::Paper,
-                Outcome::Draw => Shape::Rock,
-                Outcome::Lose => Shape::Scissors,
-            },
-            Shape::Paper => match outcome {
-                Outcome::Win => Shape::Scissors,
-                Outcome::Draw => Shape::Paper,
-                Outcome::Lose => Shape::Rock,
-            },
-            Shape::Scissors => match outcome {
-                Outcome::Win => Shape::Rock,
-                Outcome::Draw => Shape::Scissors,
-                Outcome::Lose => Shape::Paper,
-            },
+        let ours = match (theirs, &outcome) {
+            (Shape::Rock, Outcome::Win) => Shape::Paper,
+            (Shape::Rock, Outcome::Draw) => Shape::Rock,
+            (Shape::Rock, Outcome::Lose) => Shape::Scissors,
+            (Shape::Paper, Outcome::Win) => Shape::Scissors,
+            (Shape::Paper, Outcome::Draw) => Shape::Paper,
+            (Shape::Paper, Outcome::Lose) => Shape::Rock,
+            (Shape::Scissors, Outcome::Win) => Shape::Rock,
+            (Shape::Scissors, Outcome::Draw) => Shape::Scissors,
+            (Shape::Scissors, Outcome::Lose) => Shape::Paper,
         };
-        score = score + outcome as i64 + ours as i64;
+        score = score + outcome as usize + ours.score();
     }
     score
 }
